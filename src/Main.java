5bbab2eed9cff55d09b337cae513c34384f8509a
kauzarc <jujurolland91@gmail.com>
2020-04-20 16:30:16 +0200

    javafx added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
         System.out.println("Hello world !");
+        stage.show();
     }
 }

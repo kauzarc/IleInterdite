4937674bf0bafdcfb792a22723baa080acdbf495
kauzarc <jujurolland91@gmail.com>
2020-04-30 11:20:27 +0200

    reset before branch testing

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-//test
-
 public class Main extends Application {
-    //test
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println("Hello world !");
         stage.show();
     }
-
-
 }

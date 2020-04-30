0d9275de5f85510ed67c86a6b70b8a99827604d8
kauzarc <jujurolland91@gmail.com>
2020-04-30 11:07:59 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+//test
+
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {

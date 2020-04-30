f15d7480164abd63eafcb5a5a15c6eec067a257a
kauzarc <45573975+kauzarc@users.noreply.github.com>
2020-04-30 14:57:32 +0200

    JUnit test set up

--- test/modele/AbstractZoneTest.java
+++ test/modele/AbstractZoneTest.java
@@ -4,22 +4,18 @@ import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
-public class BoardTest extends TestCase {
-    Board b;
+public class AbstractZoneTest extends TestCase {
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
-        b = new Board(10, 10);
     }
 
     @Test
-    public void testTestFillBoard() {
+    public void testTestToString() {
     }
 
     @Test
-    public void testTestToString1() {
+    public void testCanReceiveWater() {
     }
 }

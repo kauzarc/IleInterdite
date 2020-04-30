3056fd35eac616df8dd1ad0af2833ba3a543290c
lucas <picasso.foissey@gmail.com>
2020-04-30 12:48:27 +0200

    kml

--- src/modele/NormalZone.java
+++ src/modele/NormalZone.java
@@ -1,7 +1,7 @@
 package modele;
 
-public class Normal_zone extends AbstractZone{
-    public Normal_zone (int x, int y){
+public class NormalZone extends AbstractZone{
+    public NormalZone (int x, int y){
         this.x = x;
         this.y = y;
         this.waterState = WaterState.normal;

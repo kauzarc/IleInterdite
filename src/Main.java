66618321c11eaaf29de6fc3811587ab01d6ebe88
lucas <picasso.foissey@gmail.com>
2020-04-30 11:04:16 +0200

    test

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    //test
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println("Hello world !");
         stage.show();
     }
+
+
 }

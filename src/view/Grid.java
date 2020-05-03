f79359bec423618c4df86d760488f9a3614c59cd
kauzarc <jujurolland91@gmail.com>
2020-05-03 23:12:31 +0200

    Grid completed

--- src/view/Grid.java
+++ src/view/Grid.java
@@ -12,10 +12,10 @@ public class Grid extends Canvas implements Observer {
     final static int SCALE = 50;
     final static Color NORMAL = Color.BROWN, FLOODED = Color.BLUE, SUBMERGED = Color.DARKBLUE;
 
-    private Board board;
+    private final Board board;
 
     public Grid(Board board) {
-        super((double)(board.getSizeX() * SCALE), (double)(board.getSizeY() * SCALE));
+        super(board.getSizeX() * SCALE, board.getSizeY() * SCALE);
         this.board = board;
         this.board.addObserver(this);
 
@@ -27,11 +27,18 @@ public class Grid extends Canvas implements Observer {
         GraphicsContext gc = getGraphicsContext2D();
         for (int x = 0; x < board.getSizeX(); x++) {
             for (int y = 0; y < board.getSizeY(); y++) {
-                if (this.board.getAt(x, y).getWaterState() == WaterState.normal)
-                {
-                    gc.setFill(NORMAL);
+                WaterState waterState = this.board.getAt(x, y).getWaterState();
+                switch (waterState) {
+                    case normal:
+                        gc.setFill(NORMAL);
+                        break;
+                    case submerged:
+                        gc.setFill(SUBMERGED);
+                        break;
+                    case flooded:
+                        gc.setFill(FLOODED);
                 }
-                gc.fillRect(x * SCALE, y * SCALE, SCALE, SCALE);
+                gc.fillRect(x * SCALE + 1, y * SCALE + 1, SCALE - 2, SCALE - 2);
             }
         }
     }
--- src/view/ViewDirector.java
+++ src/view/ViewDirector.java
@@ -6,12 +6,18 @@ import javafx.stage.Stage;
 import modele.Board;
 
 public class ViewDirector {
-    private Stage mainWindow;
+    private final Stage mainWindow;
+    private final Grid grid;
 
-    public ViewDirector(Stage mainWindow, Board board){
+    public ViewDirector(Stage mainWindow, Board board) {
         this.mainWindow = mainWindow;
+        this.grid = new Grid(board);
 
-        Scene scene = new Scene(new Group(new Grid(board)));
+        Group root = new Group();
+        root.getChildren().add(this.grid);
+
+        Scene scene = new Scene(root);
+
         mainWindow.setScene(scene);
         mainWindow.show();
     }

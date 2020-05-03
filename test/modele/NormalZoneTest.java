1e7fab05cd5ab36895c6e8e36b9677e252db463f
kauzarc <jujurolland91@gmail.com>
2020-05-03 23:28:53 +0200

    unit test on NormalZone

--- test/modele/NormalZoneTest.java
+++ test/modele/NormalZoneTest.java
@@ -1,21 +1,26 @@
 package modele;
 
-import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class NormalZoneTest extends TestCase {
+public class NormalZoneTest {
+    NormalZone zone;
 
     @Before
-    public void setUp() throws Exception {
-        super.setUp();
+    public void setUp() {
+        this.zone = null;
     }
 
     @Test
     public void testTestToString() {
+        zone = new NormalZone(0, 0);
+        Assert.assertEquals("#", zone.toString());
     }
 
     @Test
     public void testCanReceiveWater() {
+        zone = new NormalZone(0, 0);
+        Assert.assertTrue(zone.canReceiveWater());
     }
 }

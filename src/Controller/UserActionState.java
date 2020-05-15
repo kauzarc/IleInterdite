f5834833d0bf499b56b6f106a283ebb5774a50bd
kauzarc <jujurolland91@gmail.com>
2020-05-15 17:42:54 +0200

    unit test on NormalZone

--- src/Controller/UserActionState.java
+++ src/Controller/UserActionState.java
@@ -1,13 +1,41 @@
 package Controller;
 
 public class UserActionState {
-    private static UserActionState instance = new UserActionState();
+    public enum Mod {
+        moving, drying
+    }
 
-    private UserActionState(){
+    private Mod mod;
+    private int actionCount;
 
+    private static final UserActionState instance = new UserActionState();
+
+    private UserActionState() {
+        this.mod = Mod.moving;
+        this.actionCount = 0;
     }
 
     public static UserActionState getInstance() {
         return instance;
+    }
+
+    public Mod getMod() {
+        return this.mod;
+    }
+
+    public void setMod(Mod mod) {
+        this.mod = mod;
+    }
+
+    public void actionCountUp() {
+        this.actionCount++;
+    }
+
+    public void actionCountReset() {
+        this.actionCount = 0;
+    }
+
+    public int getActionCount() {
+        return this.actionCount;
     }
 }

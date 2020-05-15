cc882f39af5f955141861d2fdf9aee9dfd002654
pierre <pierre.garcia@u-psud.fr>
2020-05-15 15:02:16 +0200

    Character

---
+++ src/modele/Character.java
@@ -0,0 +1,13 @@
+package modele;
+
+public abstract class Character {
+    protected int x;
+    protected int y;
+
+    public Character(int x,int y){
+        this.x = x;
+        this.y = y;
+    }
+
+
+}

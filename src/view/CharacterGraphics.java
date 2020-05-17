afc1513dc44f64885f0568d9fc95dcc9519ae8fd
lucas <picasso.foissey@gmail.com>
2020-05-17 15:39:33 +0200

    amelioration de graphiqueCharacter

--- src/view/CharacterGraphics.java
+++ src/view/CharacterGraphics.java
@@ -3,24 +3,32 @@ package view;
 import ObserverObservable.Observer;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Translate;
 import modele.Character;
 import modele.NoramlCaracter;
 
 public class CharacterGraphics extends Rectangle implements Observer {
     private Character character;
     private int scale;
+    private int previousX;
+    private int previousY;
 
     public CharacterGraphics (Character concernedCharacter, int SCALE){
         super(SCALE * concernedCharacter.getX()+1,SCALE * concernedCharacter.getY()+1,(SCALE/2) -2, (SCALE/2) -2);
+        this.character.addObserver(this);
         this.character = concernedCharacter;
         this.scale = SCALE;
+        this.previousX = this.character.getX();
+        this.previousY = this.character.getY();
         this.update();
     }
 
     @Override
     public void update() {
-        this.setX(this.character.getX() * this.scale + 1);
-        this.setY(this.character.getY() * this.scale + 1);
+        int  lateralMove = this.scale * (this.character.getX() - this.previousX);
+        int horizontalMove = this.scale * (this.character.getY() - this.previousY);
+        Translate translate = new Translate(lateralMove, horizontalMove);
+        this.getTransforms().addAll(translate);
         this.setFill(Color.BLACK);
     }
 }

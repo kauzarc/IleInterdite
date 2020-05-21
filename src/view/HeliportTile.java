688afbc8e2aa26e44e5f3c017448f4277de0cace
kauzarc <45573975+kauzarc@users.noreply.github.com>
2020-05-21 17:51:42 +0200

    Heliport showing

--- src/view/HeliportTile.java
+++ src/view/HeliportTile.java
@@ -1,10 +1,22 @@
 package view;
 
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import modele.HeliportZone;
 
-public class HeliportTile extends AbstractTile{
+public class HeliportTile extends AbstractTile {
+    private final Text text;
+
     public HeliportTile(HeliportZone actualZone, int SCALE) {
         super(actualZone, SCALE);
+        this.text = new Text(SCALE * actualZone.getX() + 1, SCALE * actualZone.getY() + SCALE, "H");
+        this.text.setFont(new Font(50));
+        getChildren().add(this.text);
         update();
+    }
+
+    @Override
+    public void update() {
+        super.update();
     }
 }
